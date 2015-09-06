324c99ddb61f9469cd99fddfe02dc7793462dc36
Jacek135 <jac.poczatek@gmail.com>
2015-09-06 11:41:28 +0200

    End of REST Web Services 16

--- src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
@@ -14,14 +14,33 @@ public class MessageService {
 
     private Map<Long, Message> messages = DatabaseClass.getMessages();
 
-    public List<Message> getAllMessages(){
-    Message m1 = new Message(1L, "Hello World!", "Koushik");
-    Message m2 = new Message(2L, "Hello Krakow!", "Jacek");
+    public MessageService(){
+        messages.put(1L, new Message(1, "Hello World", "koushik"));
+        messages.put(2L, new Message(2, "Hello Jacek", "KARAS"));
+    }
 
-        List<Message> list = new ArrayList<>();
-        list.add(m1);
-        list.add(m2);
+    public List<Message> getAllMessages() {
+        return new ArrayList<Message>(messages.values());
+    }
 
-        return list;
+    public Message getMessage(long id){
+        return messages.get(id);
+    }
+    public Message addMessage(Message message){
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message){
+        if(message.getId() <= 0){
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id){
+        return messages.remove(id);
     }
 }
