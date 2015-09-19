528d4638e42cdaa5b417dc0b71cd28cca4cc6810
Jacek135 <jac.poczatek@gmail.com>
2015-09-19 15:38:02 +0200

    REST-24

--- src/main/java/org/koushik/javabrains/messenger/resources/InjectDemoResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/InjectDemoResource.java
@@ -1,7 +1,10 @@
 package org.koushik.javabrains.messenger.resources;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+        import javax.ws.rs.*;
+        import javax.ws.rs.core.Context;
+        import javax.ws.rs.core.HttpHeaders;
+        import javax.ws.rs.core.MediaType;
+        import javax.ws.rs.core.UriInfo;
 
 /**
  * Created by jacek on 19.09.15.
@@ -16,7 +19,14 @@ public class InjectDemoResource {
     public String etParamsUsingAnnotations(@MatrixParam("param")String matrixParam,
                                            @HeaderParam("ala") String header,
                                            @CookieParam("JSESSIONID") String cookie){
-    return "Matrix param: " + matrixParam + "; Header Value = " + header + "; Cookie = " + cookie;
-}
+        return "Matrix param: " + matrixParam + "; Header Value = " + header + "; Cookie = " + cookie;
+    }
 
+    @GET
+    @Path("context")
+    public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers ){
+        String path = uriInfo.getAbsolutePath().toString();
+        String cookies = headers.getCookies().toString();
+        return "Path : " + path + "\nCookies = " + cookies;
+    }
 }
